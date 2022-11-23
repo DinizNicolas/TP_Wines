fd637bc73707b8ef155fc55f7045adbb6342cc6b
Nicolas Diniz <diniznicol@cy-tech.fr>
2022-11-23 14:09:13 +0100

    read csv

--- main.py
+++ main.py
@@ -5,5 +5,12 @@ from csv import reader
 with open('Wines.csv','r') as read_obj:
     csv_reader = reader(read_obj)
     header = next(csv_reader)
+    inputs_ = []
+    outputs_ = []
 
     for row in csv_reader:
+        print(row)
+        list_values_tmp = [float(value) for value in row]
+        outputs_.append(list_values_tmp[-2])
+        inputs_.append(list_values_tmp[0:-2])
+
